eb0bd39c81077cab03ef190bade790b09dec8bc5
Octavian  Neamțu <oneamtu89@gmail.com>
2023-09-16 12:31:25 +0000

    Add Convolution model.

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -1,20 +1,41 @@
 import torch
 
-
 class CNNClassifier(torch.nn.Module):
-    def __init__(self):
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, stride=1):
+            """
+            2 layers of Conv2d
+            """
+            super().__init__()
+            self.block = torch.nn.Sequential(
+              torch.nn.Conv2d(n_input, n_output, kernel_size=3, padding=1, stride=stride),
+              torch.nn.ReLU(),
+              torch.nn.Conv2d(n_output, n_output, kernel_size=3, padding=1),
+              torch.nn.ReLU()
+            )
+
+        def forward(self, x):
+            return self.block(x)
+
+    def __init__(self, layers = [32, 64, 128], n_input_channels = 3):
         """
-        Your code here
+        Using blocks of Conv2d
         """
-        raise NotImplementedError('CNNClassifier.__init__')
+        super().__init__()
+        self.network = torch.nn.Sequential(*[
+            self.Block(i, o, stride=2) for i, o in zip([n_input_channels, *layers[:-1]], layers)
+        ])
+        self.classifier = torch.nn.Linear(layers[-1], 6)
 
     def forward(self, x):
         """
-        Your code here
         @x: torch.Tensor((B,3,64,64))
         @return: torch.Tensor((B,6))
         """
-        raise NotImplementedError('CNNClassifier.forward')
+        y = self.network(x)
+        # Global average pooling
+        y = y.mean(dim=[2,3])
+        return self.classifier(y)[:,0]
 
 
 def save_model(model):
