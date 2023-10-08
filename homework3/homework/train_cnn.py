4086dc9c6d64ef9dcbe28c9cead0134f465c56a0
Octavian  Neamțu <oneamtu89@gmail.com>
2023-10-08 14:48:43 +0000

    Augment data with flips and random crops.

--- homework3/homework/train_cnn.py
+++ homework3/homework/train_cnn.py
@@ -21,7 +21,13 @@ import numpy as np
 # Dropout
 # Data augmentations (Both geometric and color augmentations are important. Be aggressive here. Different levels of supertux have radically different lighting.)
 # Weight regularization
-# Early stopping
+
+# Early stopping -- helpful for capturing max
+# LR schedule - helpful to stabilize convergence; try higher LR default to see if faster convergence.
+# !python3 -m homework.train_cnn --log_dir log_bn_residual_lr_sched --batch_size 128 --epochs 60 --patience 15 --cuda True
+# Stopping at epoch 34: max accuracy 0.9091517857142857
+# !python3 -m homework.train_cnn --log_dir log_bn_residual_lr_sched --batch_size 128 --epochs 60 --patience 15 --cuda True
+# Stopping at epoch 39: max accuracy 0.8911830357142857 start_lr: 0.01
 
 def train(args):
     """
@@ -37,7 +43,8 @@ def train(args):
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
         valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
 
-    training_data = load_data('data/train', batch_size=args.batch_size)
+    training_data = load_data('data/train', batch_size=args.batch_size, 
+                              random_crop=(32, 32), random_horizontal_flip=True)
     validation_data = load_data('data/valid')
 
     optimizer = optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.999))
@@ -97,7 +104,7 @@ def train(args):
         train_logger.add_scalar('lr', optimizer.param_groups[0]['lr'], global_step)
         scheduler.step(validation_accuracy)
 
-        print(f"Epoch {epoch+1}/{args.epochs} | Train Loss: {total_loss} LR: {optimizer.param_groups[0]['lr']} " +
+        print(f"Epoch {epoch+1}/{args.epochs} | Train Loss: {total_loss} | LR: {optimizer.param_groups[0]['lr']} " +
               f"| Train Accuracy: {train_accuracy} | Validation Accuracy: {validation_accuracy}")
 
         # Early stopping - needs patience
--- homework3/homework/utils.py
+++ homework3/homework/utils.py
@@ -15,7 +15,7 @@ DENSE_LABEL_NAMES = ['background', 'kart', 'track', 'bomb/projectile', 'pickup/n
 DENSE_CLASS_DISTRIBUTION = [0.52683655, 0.02929112, 0.4352989, 0.0044619, 0.00411153]
 
 class SuperTuxDataset(Dataset):
-    def __init__(self, dataset_path):
+    def __init__(self, dataset_path, random_crop=None, random_horizontal_flip=False):
         """
         Your code here
         Hint: Use the python csv library to parse labels.csv
@@ -23,6 +23,9 @@ class SuperTuxDataset(Dataset):
         WARNING: Do not perform data normalization here. 
         """
         self.dataset_path = dataset_path
+        self.random_crop = random_crop
+        self.random_horizontal_flip = random_horizontal_flip
+        self.transform = self.get_transform()
 
         with open(f'{dataset_path}/labels.csv') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
@@ -42,9 +45,18 @@ class SuperTuxDataset(Dataset):
         Your code here
         return a tuple: img, label
         """
-        image = read_image(f'{self.dataset_path}/{self.items[idx][0]}')
-        float_image = transforms.functional.convert_image_dtype(image, torch.float32)
-        return (float_image, self.items[idx][1])
+        raw_image = read_image(f'{self.dataset_path}/{self.items[idx][0]}')
+        image = self.transform(raw_image)
+        return (image, self.items[idx][1])
+    
+    def get_transform(self):
+        transform = []
+        if self.random_crop is not None:
+            transform.append(transforms.RandomResizedCrop(self.random_crop, antialias=True))
+        if self.random_horizontal_flip:
+            transform.append(transforms.RandomHorizontalFlip())
+        transform.append(transforms.ConvertImageDtype(torch.float32))
+        return transforms.Compose(transform)
 
 
 class DenseSuperTuxDataset(Dataset):
@@ -135,8 +147,9 @@ class ConfusionMatrix(object):
         return self.matrix / (self.matrix.sum(1, keepdims=True) + 1e-5)
 
 if __name__ == '__main__':
-    dataset = DenseSuperTuxDataset('dense_data/train', transform=dense_transforms.Compose(
-        [dense_transforms.RandomHorizontalFlip(), dense_transforms.ToTensor()]))
+    dataset = SuperTuxDataset('data/train', random_crop=(32, 32), random_horizontal_flip=True)
+    # dataset = DenseSuperTuxDataset('dense_data/train', transform=dense_transforms.Compose(
+    #     [dense_transforms.RandomHorizontalFlip(), dense_transforms.ToTensor()]))
     from pylab import show, imshow, subplot, axis
 
     for i in range(15):
@@ -145,12 +158,12 @@ if __name__ == '__main__':
         imshow(F.to_pil_image(im))
         axis('off')
         subplot(5, 6, 2 * i + 2)
-        imshow(dense_transforms.label_to_pil_image(lbl))
+        # imshow(dense_transforms.label_to_pil_image(lbl))
         axis('off')
     show()
     import numpy as np
 
     c = np.zeros(5)
-    for im, lbl in dataset:
-        c += np.bincount(lbl.view(-1), minlength=len(DENSE_LABEL_NAMES))
+    # for im, lbl in dataset:
+    #     c += np.bincount(lbl.view(-1), minlength=len(DENSE_LABEL_NAMES))
     print(100 * c / np.sum(c))
