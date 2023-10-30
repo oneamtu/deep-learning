62ada7d9d073c43a141c534d948d3a88b438a984
Octavian  Neamțu <oneamtu89@gmail.com>
2023-10-30 04:29:35 +0000

    Fix img log.

--- homework4/homework/train.py
+++ homework4/homework/train.py
@@ -156,7 +156,7 @@ def log(logger, img, gt_det, det, global_step):
     det: predicted object-center heatmaps
     global_step: iteration
     """
-    logger.add_images('image', [img, gt_det, torch.sigmoid(det)], global_step)
+    logger.add_images('image', torch.tensor([img, gt_det, torch.sigmoid(det)]), global_step)
 
 if __name__ == '__main__':
     import argparse
